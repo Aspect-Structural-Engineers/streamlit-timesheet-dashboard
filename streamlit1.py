ed38239ff8487ad473435791cec8ececaffb1b33
Shlok Verma <shlok@aspectengineers.com>
2025-12-22 11:06:15 -0500

    Updates

--- streamlit1.py
+++ streamlit1.py
@@ -269,7 +269,7 @@ flex_pto = totals_by_util.loc[totals_by_util["Utilization Category"] == "Add'l &
 
 import streamlit.components.v1 as components
 
-col_left, col_right, col_spacer, col_charts = st.columns([1, 1, 0.5, 1])
+col_left, col_right, col_spacer, col_charts = st.columns([1, 1, 0.5, 0.5])
 
 
 with col_left:
